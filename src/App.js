25f2574109c5ad66049eb15bc1dd933338bf5a35
B-lovedth <greatsoma2019@gmail.com>
2022-04-23 13:02:06 +0100

    sub commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
    <div>
-    
+    <h1>yo</h1>
     </div> 
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    
+  return (
+    <div>
+      <nav className="navbar">
+          <div className="navbar-container">
+              
+          </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
