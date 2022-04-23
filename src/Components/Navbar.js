daf91e70274fc96a15862a9c2dc040660384ab72
B-lovedth <greatsoma2019@gmail.com>
2022-04-23 14:02:21 +0100

    sub commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,6 +7,10 @@ const Navbar = () => {
      const handleClick=()=>{
         setClick(!click)
      }
+
+     const closeMobileMenu = () =>{
+         setClick(false)
+     }
     return (
     <div>
       <nav className="navbar"> 
@@ -17,6 +21,20 @@ const Navbar = () => {
             <div className="menu-icon" onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
             </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/products' className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign-Up</Link>
+                </li>
+            </ul>
           </div>
       </nav>
     </div>
