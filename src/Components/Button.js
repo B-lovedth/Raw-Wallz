85d5c26f7731b87df7b92fe4712f6c5ffd34f2e2
B-lovedth <greatsoma2019@gmail.com>
2022-04-25 22:37:01 +0100

    sub commit

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -5,12 +5,12 @@ import {Link} from 'react-router-dom';
 const STYLES = ['btn--primary' , 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large']
 
-export const Button = (
+export const Button = ([
     children,
     type,
     onClick,
     buttonStyle,
-    buttonSize
+    buttonSize]
 ) =>{
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]    
