a16c4d330fa945ba7026e3a708827080fd42c587
B-lovedth <greatsoma2019@gmail.com>
2022-04-26 22:50:20 +0100

    document format

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import Navbar from "./Components/Navbar";
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+/** @format */
 
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-   <div>
-     <Router>       
-          <Navbar/>
-       <Switch>
-         <Route exact path='/' >
-            
-         </Route>
-       </Switch>
-     </Router>
-    </div> 
-  )
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
