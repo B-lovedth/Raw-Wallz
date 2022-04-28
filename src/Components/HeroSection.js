aab5fc887202b035dc2a9964912c121889d3ef91
B-lovedth <greatsoma2019@gmail.com>
2022-04-28 21:25:28 +0100

    sub commit

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,8 +1,17 @@
+import { Button } from "./Button"
 
 const HeroSection = () => {
   return (
-    <div>
-      
+    <div className = 'hero-container'>
+      <video src="/public/Video/video-2.mp4" autoPlay loop muted></video>
+      <h1>Adventure Awaits</h1>
+      <p>Let's go</p>
+      <div className="hero-btns">
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+      </div>
     </div>
   )
 }
