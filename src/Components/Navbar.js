e841ec674d0dc538b3ddc45a0c0b379f960bc4b8
B-lovedth <greatsoma2019@gmail.com>
2022-04-28 15:01:24 +0100

    sub commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            SOMA <i class="fa-brands fa-sitrox fa-flip --fa-animation-duration: 3s" ></i>
+            SOMA <i class="fa-brands fa-sitrox fa-flip"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars-staggered"}></i>
