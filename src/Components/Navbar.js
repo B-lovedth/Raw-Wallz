350fff201538c734efabb429f21064e639c07933
B-lovedth <greatsoma2019@gmail.com>
2022-04-28 15:48:18 +0100

    sub commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => {
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                {/* Home */}<i class="fa-solid fa-house"></i>
+                {click ? "Home" : <i class="fa-solid fa-house"></i>}
               </Link>
             </li>
             <li className="nav-item">
@@ -52,7 +52,7 @@ const Navbar = () => {
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
-                {/* Services */}<i class="fa-solid fa-computer"></i>
+                {click ? "Services" :<i class="fa-solid fa-computer"></i>}
               </Link>
             </li>
             <li className="nav-item">
