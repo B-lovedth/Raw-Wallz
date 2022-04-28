6c2a1a5aceb6db6fe5171cecc0170f8a4a97789f
B-lovedth <greatsoma2019@gmail.com>
2022-04-28 15:18:46 +0100

    sub commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import './App.css'
+import './App.css'
 
 function App() {
   return (
