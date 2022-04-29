069aad434b27f101c7df6e3d4cf243f3a24f11d3
B-lovedth <greatsoma2019@gmail.com>
2022-04-29 15:50:33 +0100

    corrected video src

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,11 +1,11 @@
 import { Button } from "./Button"
 import './HeroSection.css'
-import './src/App.css'
+import '../App.css'
 
 const HeroSection = () => {
   return (
     <div className = 'hero-container'>
-      <video src="/public/Video/video-2.mp4" autoPlay loop muted/>
+      <video src="./Video/video-1.mp4" autoPlay loop muted/>
       <h1>Adventure Awaits</h1>
       <p>Let's go</p>
       <div className="hero-btns">
