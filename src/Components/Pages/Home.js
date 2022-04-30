b5691e85f627db483b8cb478bcd6e4bca866bd5e
B-lovedth <greatsoma2019@gmail.com>
2022-04-30 14:39:11 +0100

    sub commit

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+import HeroSection from '../HeroSection'
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
