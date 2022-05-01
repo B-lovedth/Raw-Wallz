47630337e10f2a6863a534ba1ced0b30fea90cec
B-lovedth <greatsoma2019@gmail.com>
2022-05-01 20:16:48 +0100

    new commit

---
+++ src/Components/Carditems.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Carditems = () => {
+  return (
+    <div>
+        <li className="cards--item">
+            <Link className='cards--item--link'>
+                <figure className="card-item-pic-wrap">
+                    <img src='/' alt='Travel image' className='card--item--img'/>
+                </figure>
+                <div className="cards--item--info">
+                    <h5 className="card--item--text"></h5>
+                </div>
+            </Link>
+        </li>
+    </div>
+  )
+}
+
+export default Carditems
---
+++ src/Components/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Carditems from './Carditems'
+
+const Cards = () => {
+  return (
+    <div className='cards'>
+      <h1>Check out these awesome Destinations</h1>
+      <div className="cards--">
+          <div className="card--item">
+              <Carditems/>
+          </div>
+      </div>
+    </div>
+  )
+}
+ className='cards'
+export default Cards
