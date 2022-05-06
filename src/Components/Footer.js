ebbc8c0d15e954ba5da7cb8a74c61bd117ff3f3a
Solomon Great <greatsoma2019@gmail.com>
2022-05-06 01:59:52 +0100

    sub commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <section className="footer-subcription">
+      <section className="footer-subscription">
           <p className="footer-subscription-heading">
               Join the Adventure newsletter to Bla blah blah
           </p>
@@ -16,16 +16,16 @@ const Footer = () => {
           </p>
           <div className="input-areas">
               <form >
-                  <input type="email" name="email" placeholder='your Email' className='footer-input' />
-                  <Button buttonStyle='btn-outline' link='/services'>
+                  <input type="email" name="email" placeholder='Your Email' className='footer-input' />
+                  <Button buttonStyle='btn--outline' link='/services'>
                     Subscribe
                   </Button>
               </form>
           </div>
       </section>
       <div className="footer-links">
-          <div className="footer-links-wrapper">
-              <div className="footer-links-items">
+          <div className="footer-link-wrapper">
+              <div className="footer-link-items">
                   <h2>About us</h2>
                   <Link to='/sign-up'>How it Works</Link>
                   <Link to= '/sign-up'>Testimonials</Link>
@@ -34,8 +34,8 @@ const Footer = () => {
                   <Link to= '/sign-up'>Terms of Service</Link>
               </div>
           </div>
-          <div className="footer-links-wrapper">
-              <div className="footer-links-items">
+          <div className="footer-link-wrapper">
+              <div className="footer-link-items">
                   <h2>Contact us</h2>
                   <Link to='/sign-up'>How it Works</Link>
                   <Link to= '/sign-up'>Testimonials</Link>
@@ -52,7 +52,27 @@ const Footer = () => {
                     <i class="fa-brands fa-sitrox fa-bounce"></i>OMA
                   </Link>
               </div>
-              <small className="website-rights">SOMA 2022</small>
+              <small className="website-rights">SOMA © 2022</small>
+              <div className="social-icons">
+                <Link className="social-icon-link facebook"
+                to='/'
+                target='blank'
+                aria-label='Facebook'>
+                    <i className="fab fa-facebook-f"></i>
+                </Link>
+                <Link className="social-icon-link youtube"
+                to='/'
+                target='blank'
+                aria-label='Youtube'>
+                    <i className="fab fa-youtube"></i>
+                </Link>
+                <Link className="social-icon-link twitter"
+                to='/'
+                target='blank'
+                aria-label='Twitter'>
+                    <i className="fab fa-Twitter"></i>
+                </Link>
+              </div>
           </div>
       </section>
     </div>
