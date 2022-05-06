0968fc34a1731932ca6a841420980e53f5556c28
Solomon Great <greatsoma2019@gmail.com>
2022-05-06 01:13:28 +0100

    sub commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from './Button'
+import './Footer.css'
 
 const Footer = () => {
   return (
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../App.css'
 import Cards from '../Cards'
+import Footer from '../Footer'
 import HeroSection from '../HeroSection'
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <HeroSection/> 
       <Cards/>
+      <Footer/>
     </div>
   )
 }
