91027ed528c554ee1be0f687b998e95936f7a72d
Solomon Great <greatsoma2019@gmail.com>
2022-05-06 23:12:25 +0100

    latest commit

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,6 +1,50 @@
 import React from 'react'
 import '../../App.css'
 
-export default function SignUp(){
-    return<h1 className='sign-up'>SIGN-UP</h1>
+function SignUp(){
+    return(
+    // <div>
+    //     <h2 className='sign-up'>SIGN-UP</h2>
+    //     <form action="#">
+    //     <form>
+    //     <label>Article title:</label>
+    //     <input
+    //       type="text"
+    //       required
+    //       value={title}
+    //       ></input>
+    //     <label>Article body:</label>
+    //     <textarea
+    //       required
+    //       value={body}
+    //       onChange={(e) => setBody(e.target.value)}
+    //     ></textarea>
+    //     <label>Article author:</label>
+    //     <select
+    //       value={author}
+    //     >
+    //       <option value="mario">mario</option>
+    //       <option value="soma">soma</option>
+    //     </select>
+    //     <input
+    //        type="text"
+    //        required
+    //        value={author}
+    //        placeholder='-Your Name-'
+    //     />
+    //     <button>Add article</button>
+    //     <button disabled>Adding article...</button>
+    //   </form>
+  
+    //     </form>
+    // </div>
+    <div>
+        <h1>Still under development</h1>
+        <p>check back later</p>
+    </div>
+    
+    )
 }
+
+
+export default SignUp
