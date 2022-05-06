8e7cecbe5790a19d3de3a647f652cff923bab5c5
Solomon Great <greatsoma2019@gmail.com>
2022-05-06 01:09:20 +0100

    sub commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Button } from './Button'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='fotter-container'>
+      <section className="footer-subcription">
+          <p className="footer-subscription-heading">
+              Join the Adventure newsletter to Bla blah blah
+          </p>
+          <p className="footer-subscription-text">
+              You can Blah blah blah
+          </p>
+          <div className="input-areas">
+              <form >
+                  <input type="email" name="email" placeholder='your Email' className='footer-input' />
+                  <Button buttonStyle='btn-outline' link='/services'>
+                    Subscribe
+                  </Button>
+              </form>
+          </div>
+      </section>
     </div>
   )
 }
