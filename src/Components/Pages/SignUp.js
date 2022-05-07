ec97471a193a95152540e28f1e2532142b5efadd
Solomon Great <greatsoma2019@gmail.com>
2022-05-07 22:25:58 +0100

    sub commit

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,24 +1,19 @@
 import React from "react";
-// import "./SignUp.css";
+import "./SignUp.css";
 
 function SignUp() {
     return (
         <div className='signup-container'>
-            <h1>definition</h1>
-            <form>
-                <label>Article title:</label>
-                <input type='text' required></input>
-                <label>Article body:</label>
-                <textarea required></textarea>
-                <label>Article author:</label>
-                <select>
-                    <option value='mario'>mario</option>
-                    <option value='soma'>soma</option>
-                </select>
-                <input type='text' required placeholder='-Your Name-' />
-                <button>Add article</button>
-            </form>
-            <h1>prop money</h1>
+            <h1>SIGN UP</h1>
+            <div className="form-container">
+                <div className="form-wrapper">
+                <form action="#">
+                    <label>Name</label>
+                    <input type="text" name="Name" id="" />
+                </form>
+                </div>
+                
+            </div>
         </div>
     );
 }
