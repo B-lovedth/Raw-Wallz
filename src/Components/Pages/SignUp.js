d244e4fe3e15b2820096ec42846481be941579d6
Solomon Great <greatsoma2019@gmail.com>
2022-05-07 21:48:59 +0100

    sub commit

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,46 +1,32 @@
 import React from 'react'
-import '../../App.css'
+import './SignUp.css'
 
 function SignUp(){
     return(
-    // <div>
-    //     <h2 className='sign-up'>SIGN-UP</h2>
-    //     <form action="#">
-    //     <form>
-    //     <label>Article title:</label>
-    //     <input
-    //       type="text"
-    //       required
-    //       value={title}
-    //       ></input>
-    //     <label>Article body:</label>
-    //     <textarea
-    //       required
-    //       value={body}
-    //       onChange={(e) => setBody(e.target.value)}
-    //     ></textarea>
-    //     <label>Article author:</label>
-    //     <select
-    //       value={author}
-    //     >
-    //       <option value="mario">mario</option>
-    //       <option value="soma">soma</option>
-    //     </select>
-    //     <input
-    //        type="text"
-    //        required
-    //        value={author}
-    //        placeholder='-Your Name-'
-    //     />
-    //     <button>Add article</button>
-    //     <button disabled>Adding article...</button>
-    //   </form>
-  
-    //     </form>
-    // </div>
     <div>
-        <h1>Still under development</h1>
-        <p>check back later</p>
+        <form>
+        <label>Article title:</label>
+        <input
+          type="text"
+          required
+          ></input>
+        <label>Article body:</label>
+        <textarea
+          required
+        ></textarea>
+        <label>Article author:</label>
+        <select
+        >
+          <option value="mario">mario</option>
+          <option value="soma">soma</option>
+        </select>
+        <input
+           type="text"
+           required
+           placeholder='-Your Name-'
+        />
+        <button>Add article</button>
+      </form>  
     </div>
     
     )
