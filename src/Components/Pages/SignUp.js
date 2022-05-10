ed98bb5d2734cb9e1b958b7269f1aebbcc1c5d22
Solomon Great <greatsoma2019@gmail.com>
2022-05-10 21:50:24 +0100

    sub commit

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -21,9 +21,8 @@ function SignUp() {
                     <input required type="text" name="Name"  placeholder="Preferred Username" />
                     <label></label>
                     <input required type="email" name="Name"  placeholder="Email" />
-                    <p id="show-p">show <i className="fab fa eye"></i></p>
+                    <p id="show-p">show<i className="fab fa-eyes"></i></p>
                     <input required type="password" name="Name" id="p1" placeholder="Password" />
-                    <input type="checkbox" onclick="myFunction()"/>
                     <input required type="password" name="" id="p2" placeholder="Confirm Password" />
                     <button type="submit" onClick={handleClick}>Sign up</button>
                 </form>
