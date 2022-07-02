941e305992af64d1dcafd39e0bdf57358fea20a0
Great Solomon <greatsoma2019@gmail.com>
2022-07-02 20:06:28 +0100

    Updated Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,4 @@
-/** @format */
-
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import "./Navbar.css";
