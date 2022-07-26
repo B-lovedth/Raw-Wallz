8e3a1712a71f7d194c75767f94d2fbd2bc53c22e
Solomon Great <101014398+B-lovedth@users.noreply.github.com>
2022-07-26 15:49:31 +0100

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <i class="fa-brands fa-sitrox fa-bounce"></i>OMA
+            <i class="fa-brands fa-r-project"></i>awWallz
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars-staggered"}> </i>
