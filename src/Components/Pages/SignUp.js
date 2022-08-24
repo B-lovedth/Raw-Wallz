e4d6872f3541ea2d1bae54d92460b885b4ef67de
B-lovedth <101014398+B-lovedth@users.noreply.github.com>
2022-08-24 20:55:15 +0100

    Mass Update

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaTwitter, FaFacebook, FaGithub, FaLinkedin, FaAt, FaIdCard} from "react-icons/fa";
 import "./SignUp.css";
+import wolf from "../signup.png"
+import yellow from "../yellow2.png"
 
 function SignUp() {
     const [email, setEmail] = useState('')
-    const [userName, setUserName] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
     const [passwordOne, setPasswordOne] = useState('')
     const [passwordTwo, setPasswordTwo] = useState('')
     const [clickOne,setClickOne] = useState(false)
     const [clickTwo,setClickTwo] = useState(false)
-    const [error, setError] = useState('')
+  const [error, setError] = useState('')
+  const [mode, setMode] = useState(false)
+  const modeHandle = () => {
+    setMode(!mode)
+    console.log(mode)
+  }
     const handleClick=()=>{
     
     }
@@ -30,55 +38,176 @@ function SignUp() {
       <div className='signup-container'>
         <div className='form-container'>
           <div className='form-wrapper'>
-            <form action='#' className='form-items'>
-              <h1>SIGN UP</h1>
-              <label>E-mail</label>
-              <input
-                required
-                type='email'
-                name='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <label>Name</label>
-              <input
-                required
-                type='text'
-                name='Name'
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-              />
-              <label>Password </label>
-              <div className='password-flex'>
-                <input
-                  required
-                  type={clickOne ? "text" : "password"}
-                  name='password'
-                  value={passwordOne}
-                  onChange={(e) => setPasswordOne(e.target.value)}
-                />
-                <span id='show-p' onClick={showPasswordOne}>
-                  {clickOne ? <FaEye /> : <FaEyeSlash />}
+            <div
+              className={
+                mode ? "sign-up-container active" : "sign-up-container"
+              }
+            >
+              <small className='small'>TRY FOR FREE</small>
+              <h1>
+                Create new account<span className='dot'>.</span>
+              </h1>
+              <div class='social-container'>
+                <span class='social'>
+                  <FaFacebook />
+                </span>
+                <span class='social'>
+                  <FaTwitter />
+                </span>
+                <span class='social'>
+                  <FaGithub />
+                </span>
+                <span class='social'>
+                  <FaLinkedin />
                 </span>
               </div>
-              <label>Confirm Password </label>
-              <div className='password-flex'>
-                <input
-                  required
-                  type={clickTwo ? "text" : "password"}
-                  name='password'
-                  value={passwordTwo}
-                  onChange={(e) => setPasswordTwo(e.target.value)}
-                />
-                <span id='show-p' onClick={showPasswordTwo}>
-                  {clickTwo ? <FaEye /> : <FaEyeSlash />}
+              <form action='#' className='form'>
+                <div className='row'>
+                  <div className='row-input'>
+                    <input
+                      required
+                      placeholder='firstname'
+                      type='text'
+                      name='firstname'
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                    <span id='show-p'>
+                      <FaIdCard />
+                    </span>
+                  </div>
+                  <div className='row-input'>
+                    <input
+                      required
+                      placeholder='lastname'
+                      type='text'
+                      name='lastname'
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                    <span id='show-p'>
+                      <FaIdCard />
+                    </span>
+                  </div>
+                </div>
+                <div className='row-input'>
+                  <input
+                    required
+                    placeholder='E-mail'
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <span id='show-p'>
+                    <FaAt />
+                  </span>
+                </div>
+                <div className='row'>
+                  <div className='password-flex'>
+                    <input
+                      required
+                      placeholder='Password'
+                      type={clickOne ? "text" : "password"}
+                      name='password'
+                      value={passwordOne}
+                      onChange={(e) => setPasswordOne(e.target.value)}
+                    />
+                    <span id='show-p' onClick={showPasswordOne}>
+                      {clickOne ? <FaEye /> : <FaEyeSlash />}
+                    </span>
+                  </div>
+                  <div className='password-flex'>
+                    <input
+                      required
+                      placeholder='Confirm password'
+                      type={clickTwo ? "text" : "password"}
+                      name='password'
+                      value={passwordTwo}
+                      onChange={(e) => setPasswordTwo(e.target.value)}
+                    />
+                    <span id='show-p' onClick={showPasswordTwo}>
+                      {clickTwo ? <FaEye /> : <FaEyeSlash />}
+                    </span>
+                  </div>
+                </div>
+                <div className='error'>{error}</div>
+                <div className='btn-wrapper'>
+                  <button type='submit' onClick={handleClick}>
+                    Sign up
+                  </button>
+                  <button onClick={() => modeHandle()}>Log-in?</button>
+                </div>
+              </form>
+            </div>
+            <div
+              className={
+                mode ? "sign-in-container" : "sign-in-container active"
+              }
+            >
+              <small className='small'>WELCOME BACK</small>
+              <h1>
+                Login to your account<span className='dot'>.</span>
+              </h1>
+              <div class='social-container'>
+                <span class='social'>
+                  <FaFacebook />
+                </span>
+                <span class='social'>
+                  <FaTwitter />
+                </span>
+                <span class='social'>
+                  <FaGithub />
+                </span>
+                <span class='social'>
+                  <FaLinkedin />
                 </span>
               </div>
-              <div className='error'>{error}</div>
-              <button onClick={handleClick}>Sign-up</button>
-            </form>
+              <form action='#' className='form'>
+                <div className='row'></div>
+                <div className='row-input'>
+                  <input
+                    required
+                    placeholder='E-mail'
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <span id='show-p'>
+                    <FaAt />
+                  </span>
+                </div>
+                <div className='row-input'>
+                  <div className='password-flex'>
+                    <input
+                      required
+                      placeholder='Password'
+                      type={clickOne ? "text" : "password"}
+                      name='password'
+                      value={passwordOne}
+                      onChange={(e) => setPasswordOne(e.target.value)}
+                    />
+                    <span id='show-p' onClick={showPasswordOne}>
+                      {clickOne ? <FaEye /> : <FaEyeSlash />}
+                    </span>
+                  </div>
+                </div>
+                <div className='error'>{error}</div>
+                <div className='btn-wrapper'>
+                  <button onClick={handleClick}>Log in</button>
+                  <button onClick={() => modeHandle()} className='mode'>
+                    Sign up?
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className='wolf'>
+              <img src={wolf} alt='wolf' />
+            </div>
           </div>
         </div>
+        <img src={yellow} alt='yellow' className='yellow'/>
       </div>
     );
 }
