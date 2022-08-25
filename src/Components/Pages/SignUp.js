f8a41953b228955537962a62677bbfa83fdbd93d
B-lovedth <101014398+B-lovedth@users.noreply.github.com>
2022-08-25 11:30:19 +0100

    Bug fixes

--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
 import "./SignUp.css";
-import Footer from '../Footer'
-import { Button } from "../Button";
+
 
 function SignUp() {
     const [click,SetClick] = useState(false)
     const p1 = document.getElementById('p1')
     const p2 = document.getElementById('p2')
-    const err = document.getElementById('error')
-    const showPassword = () =>{
-        if(click===false){
-            p1.type= "text"
-            p2.type="text"
-        }else{
-            p1.type = "password"
-            p2.type = "password"
-        }    
-        SetClick(!click)
-    }
-    const handleClick=()=>{
-        if(p1.value===p2.value){
-            err.textContent =""
+    const err = document.getElementById('error') 
+    const showPassword = () =>{ 
+        if(click===false){ 
+            p1.type= "text" 
+            p2.type="text" 
+        }else{ 
+            p1.type = "password" 
+            p2.type = "password" 
+        }     
+        SetClick(!click) 
+    } 
+    const handleClick=()=>{ 
+        if(p1.value===p2.value){ 
+            err.textContent ="" 
         }else{
             err.textContent = "The passwords are not the same,Make sure they are!"
         }
     }
     return (
         <div className='signup-container'>
-            <div className="form-container">
-                <div className="form-wrapper">
-                <form action="#" className="form-items">
-                    <h1>SIGN UP</h1>
-                    <label>E-mail</label>
-                    <input required type="email" name="Name"  placeholder="Email" />
-                    <label>Name</label>
+            <div className="form-container"> 
+                <div className="form-wrapper"> 
+                <form action="#" className="form-items"> 
+                    <h1>SIGN UP</h1> 
+                    <label>E-mail</label> 
+                    <input required type="email" name="email"  placeholder="Email" /> 
+                    <label>Name</label> 
                     <input required type="text" name="Name"  placeholder="Preferred Username" />
                     <div className="password-flex">
                         <label>Password  </label>
@@ -43,14 +42,11 @@ function SignUp() {
                     <input required type="password" name="Name" id="p1" placeholder="*********" />
                     <label>Confirm Password</label>
                     <input required type="password" name="" id="p2" placeholder="*********" />
-                    
-                    <Button onClick={handleClick}>Sign-up</Button>
+                    <button onClick={handleClick}>Sign-up</button>
                     <div id="error"></div>
-                    
                 </form>
                 </div>
             </div>
-            <Footer/>        
         </div>
     );
 }
