1e68d94fffac92fe0acbf61d2a68aaac64d427c5
B-lovedth <101014398+B-lovedth@users.noreply.github.com>
2022-08-25 14:58:12 +0100

    Sub commit

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -5,30 +5,32 @@ import '../App.css'
 const HeroSection = () => {
   return (
     <>
-    <div className = 'hero-container'>
-      <h1>Explore Amazing Images</h1>
-      <p>Let's Go</p>
-      <div className="hero-btns">
-          <Button
-            className='btns'
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'
-            link='/sign-up'
+      <div className='hero-container'>
+        <div className='hero-wrapper'>
+          <h1>Explore Amazing Images</h1>
+          <p>Let's Go</p>
+          <div className='hero-btns'>
+            <Button
+              className='btns'
+              buttonStyle='btn--outline'
+              buttonSize='btn--large'
+              link='/sign-up'
             >
               GET STARTED
             </Button>
-          <Button
-            className='btns'
-            buttonStyle='btn--primary'
-            buttonSize='btn--large'
-            link='/trailer'
+            <Button
+              className='btns'
+              buttonStyle='btn--primary'
+              buttonSize='btn--large'
+              link='/trailer'
             >
-              WATCH TRAILER <i class="fa-regular fa-circle-play"></i>
+              WATCH TRAILER <i class='fa-regular fa-circle-play'></i>
             </Button>
+          </div>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
 export default HeroSection
